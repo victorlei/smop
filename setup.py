b426b802e07311212579a9526006e07dc5373ab7
victorlei <victorlei@gmail.com>
2014-01-21 14:20:34 +0200

    Start working on 0.24

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     #url = 'http://chiselapp.com/user/victorlei/repository/smop-dev/index',
     #download_url = 'http://code.google.com/p/smop-dev/downloads/list',
     name = 'smop',
-    version = '0.23',
+    version = '0.24',
     entry_points = { 'console_scripts': [ 'smop = smop.main:main', ], },
     packages = ['smop'],
     #package_dir = {'':'src'},
