fa07b94fb758bbced119dd53418a50c007a30ca3
adriaanph <adriaan.peenshough@gmail.com>
2015-03-14 23:01:44 +0200

    2to3.py

--- smop/resolve.py
+++ smop/resolve.py
@@ -1,3 +1,16 @@
+# smop -- Matlab to Python compiler
+# Copyright 2011-2013 Victor Leikehman
+"""
+if i.defs: 
+    i is defined, possibly more than once.  
+    Typical for vairable references.
+
+if i.defs is None:
+    i is a definition (lhs)
+    
+if i.defs == set():
+    i is used but not defined.  
+    Typical for function calls.
 
 symtab is a temporary variable, which maps
 variable names (strings) to sets of ident
