75659364ee0257077a58a243c785d1cb395905ec
xoviat <xoviat@users.noreply.github.com>
2017-10-02 18:09:42 -0500

    recipes: fix invalid function object

--- smop/recipes.py
+++ smop/recipes.py
@@ -134,7 +134,7 @@ def recordtype(typename, field_names, verbose=False, **default_kwds):
     if PY2:
         cls.__init__.im_func.func_defaults = init_defaults
     else:
-        cls.__init__.__func__.__defaults__ = init_defaults
+        cls.__init__.__defaults__ = init_defaults
     # For pickling to work, the __module__ variable needs to be set to the frame
     # where the named tuple is created.  Bypass this step in enviroments where
     # sys._getframe is not defined (Jython for example).
